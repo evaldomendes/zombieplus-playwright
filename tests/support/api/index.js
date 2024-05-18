9d9cf514c91bcb9e1f5f8cf5911e3373e1d3fcfb
Evaldo <evaldo.santos@fpf.br>
2024-05-18 19:50:50 -0400

    Adicionando variáveis.

--- tests/support/api/index.js
+++ tests/support/api/index.js
@@ -1,14 +1,16 @@
 const { expect } = require('@playwright/test')
+require('dotenv').config()
 
 export class Api {
 
     constructor(request) {
         this.request = request
         this.token = undefined
+        this.baseApi = process.env.BASE_API
     }
 
     async setToken() {
-        const response = await this.request.post('http://localhost:3333/sessions', {
+        const response = await this.request.post(this.baseApi+'/sessions', {
             data: {
                 email: 'admin@zombieplus.com',
                 password: 'pwd123'
@@ -21,7 +23,7 @@ export class Api {
     }
 
     async getCompanyIdByName(companyName) {
-        const response = await this.request.get('http://localhost:3333/companies', {
+        const response = await this.request.get(this.baseApi+'/companies', {
             headers: {
                 Authorization: this.token
             },
@@ -40,7 +42,7 @@ export class Api {
 
         const companyId = await this.getCompanyIdByName(movie.company)
 
-        const response = await this.request.post('http://localhost:3333/movies', {
+        const response = await this.request.post(this.baseApi+'/movies', {
             headers: {
                 Authorization: this.token,
                 ContentType: 'multipart/form-data',
@@ -62,7 +64,7 @@ export class Api {
 
         const companyId = await this.getCompanyIdByName(tvshow.company)
 
-        const response = await this.request.post('http://localhost:3333/tvshows', {
+        const response = await this.request.post(this.baseApi+'/tvshows', {
             headers: {
                 Authorization: this.token,
                 ContentType: 'multipart/form-data',
--- tests/support/database.js
+++ tests/support/database.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg')
+require ('dotenv').config()
 
 const DbConfig = {
-    user: 'postgres',
-    host: 'postgres',
-    database: 'playwright_zombie',
-    password: '123456',
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 }
 
 export async function executeSQL(sqlScript) {
